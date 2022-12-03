ea8252a0cdc74362663c74c327ba861b5ad45559
Nikodem Rabuliński <nikodem@rabulinski.com>
2022-12-03 12:49:33 +0100

    Day 3

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    
+
     # Languages for AoC
     # roc = {
     #   url = "github:roc-lang/roc";
@@ -17,7 +17,11 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    
+    min-src = {
+      url = "github:h3rald/min/v0.37.0";
+      flake = false;
+    };
+
     # Build helpers
     crane = {
       url = "github:ipetkov/crane";
@@ -37,7 +41,7 @@
         flake-utils.follows = "flake-utils";
       };
     };
-    
+
     # Other
     nur = {
       url = "github:nrabulinski/nur-packages";
@@ -53,22 +57,42 @@
     nixpkgs,
     flake-parts,
     nur,
+    min-src,
     # roc,
     ...
   }:
     flake-parts.lib.mkFlake {inherit self;} {
+      # Other systems will probably not work
       systems = [
         "x86_64-linux"
+        "x86_64-darwin"
         "aarch64-darwin"
       ];
 
-      perSystem = {pkgs, inputs', system, ...}: let
+      perSystem = {
+        pkgs,
+        inputs',
+        system,
+        ...
+      }: let
         inherit (pkgs) lib;
+        inherit (pkgs.stdenv) isDarwin isAarch64 isx86_64;
         pkgs' = import nixpkgs {
           inherit system;
           config.allowBroken = true;
           overlays = [
             nur.overlay
+            (final: prev: {
+              x86-64 =
+                if isx86_64
+                then final
+                else if isDarwin && isAarch64
+                then (import nixpkgs {
+                  system = "x86_64-darwin";
+                  overlays = [ nur.overlay ];
+                })
+                else {};
+            })
             # (final: prev: {
             #   zig = final.zig_0_10;
             # })
@@ -80,16 +104,17 @@
         # };
       in {
         formatter = pkgs.alejandra;
-        
+
         devShells.default = pkgs.mkShell {
-          packages = [
-            pkgs.just
-            pkgs'.racket
+          packages = with pkgs'; [
+            just
+            racket
+            x86-64.min-lang
             # roc'
           ];
 
-          nativeBuildInputs = with pkgs; [ pkg-config ];
-          buildInputs = lib.optional pkgs.stdenv.isDarwin [ pkgs.libiconv ];
+          nativeBuildInputs = with pkgs; [pkg-config];
+          buildInputs = lib.optional pkgs.stdenv.isDarwin [pkgs.libiconv];
         };
       };
     };
